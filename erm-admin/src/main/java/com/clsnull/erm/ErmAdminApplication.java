18de2d6c6d1cc50631a0eb0df5449a28543bcf3f
clsnull <1334084021@qq.com>
2023-07-16 23:48:20 +0800

    init project

---
+++ erm-admin/src/main/java/com/clsnull/erm/ErmAdminApplication.java
@@ -0,0 +1,13 @@
+package com.clsnull.erm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErmAdminApplication.class, args);
+    }
+
+}
---
+++ erm-admin/src/test/java/com/clsnull/erm/ErmAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clsnull.erm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErmAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
