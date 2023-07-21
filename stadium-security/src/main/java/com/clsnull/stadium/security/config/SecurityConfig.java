7de419f371e24ac8d62c267da912bdfab54fd2f7
clsnull <1334084021@qq.com>
2023-07-21 18:55:48 +0800

    'security'

---
+++ stadium-security/src/main/java/com/clsnull/stadium/security/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.clsnull.stadium.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
